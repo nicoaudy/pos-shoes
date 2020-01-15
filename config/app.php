bc9edbc2b4830ee54266e2c9372f8679a2edfa64
Nico Audy <nicoaudy1994@gmail.com>
2020-01-15 11:07:16 +0700

    initial dropbox setup

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\DropboxServiceProvider::class,
 
         /*
          * Application Service Providers...
