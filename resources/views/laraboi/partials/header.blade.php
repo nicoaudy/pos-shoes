1b329fc69fa548ddcd2f65750a00b0f77a16e892
Nico Audy <nicoaudy1994@gmail.com>
2020-08-05 10:37:17 +0700

    header update

--- resources/views/laraboi/partials/header.blade.php
+++ resources/views/laraboi/partials/header.blade.php
@@ -2,51 +2,13 @@
 	<div class="container-lg wide-xl">
 		<div class="nk-header-wrap">
 			<div class="nk-header-brand">
-				<a href="html/subscription/index.html" class="logo-link">
+				<a href="{{ route('home') }}" class="logo-link">
 					<img class="logo-light logo-img" src="{{ asset('images/logo.png') }}" srcset="{{ asset('images/logo.png') }}" alt="logo">
 					<img class="logo-dark logo-img" src="{{ asset('images/logo.png') }}" srcset="{{ asset('images/logo.png') }}" alt="logo-dark">
 				</a>
 			</div>
 			<div class="nk-header-tools">
 				<ul class="nk-quick-nav">
-					<li class="dropdown user-dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<div class="user-toggle">
-								<div class="user-avatar sm">
-									<em class="icon ni ni-user-alt"></em>
-								</div>
-								<div class="user-name dropdown-indicator d-none d-sm-block">Abu Bin Ishityak</div>
-							</div>
-						</a>
-						<div class="dropdown-menu dropdown-menu-md dropdown-menu-right dropdown-menu-s1">
-							<div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
-								<div class="user-card">
-									<div class="user-avatar">
-										<span>AB</span>
-									</div>
-									<div class="user-info">
-										<span class="lead-text">Abu Bin Ishtiyak</span>
-										<span class="sub-text">info@softnio.com</span>
-									</div>
-									<div class="user-action">
-										<a class="btn btn-icon mr-n2" href="html/subscription/profile-setting.html"><em class="icon ni ni-setting"></em></a>
-									</div>
-								</div>
-							</div>
-							<div class="dropdown-inner">
-								<ul class="link-list">
-									<li><a href="html/subscription/profile.html"><em class="icon ni ni-user-alt"></em><span>View Profile</span></a></li>
-									<li><a href="html/subscription/profile-setting.html"><em class="icon ni ni-setting-alt"></em><span>Account Setting</span></a></li>
-									<li><a href="html/subscription/profile-activity.html"><em class="icon ni ni-activity-alt"></em><span>Login Activity</span></a></li>
-								</ul>
-							</div>
-							<div class="dropdown-inner">
-								<ul class="link-list">
-									<li><a href="#"><em class="icon ni ni-signout"></em><span>Sign out</span></a></li>
-								</ul>
-							</div>
-						</div>
-					</li>
 					<li class="dropdown notification-dropdown">
 						<a href="#" class="dropdown-toggle nk-quick-nav-icon mr-lg-n1" data-toggle="dropdown">
 							<div class="icon-status icon-status-info"><em class="icon ni ni-bell"></em></div>
@@ -116,6 +78,54 @@
 							</div>
 							<div class="dropdown-foot center">
 								<a href="#">View All</a>
+							</div>
+						</div>
+					</li>
+					<li class="dropdown user-dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<div class="user-toggle">
+								<div class="user-avatar sm">
+									<em class="icon ni ni-user-alt"></em>
+								</div>
+								<div class="user-name dropdown-indicator d-none d-sm-block">{{ ucfirst(auth()->user()->name) }}</div>
+							</div>
+						</a>
+						<div class="dropdown-menu dropdown-menu-md dropdown-menu-right dropdown-menu-s1">
+							<div class="dropdown-inner user-card-wrap bg-lighter d-none d-md-block">
+								<div class="user-card">
+									<div class="user-avatar">
+										<span>AB</span>
+									</div>
+									<div class="user-info">
+										<span class="lead-text">{{ ucfirst(auth()->user()->name) }}</span>
+										<span class="sub-text">{{ auth()->user()->email }}</span>
+									</div>
+									<div class="user-action">
+										<a class="btn btn-icon mr-n2" href="#"><em class="icon ni ni-setting"></em></a>
+									</div>
+								</div>
+							</div>
+							<div class="dropdown-inner">
+								<ul class="link-list">
+									<li><a href="#"><em class="icon ni ni-user-alt"></em><span>View Profile</span></a></li>
+									<li><a href="#"><em class="icon ni ni-setting-alt"></em><span>Account Setting</span></a></li>
+									<li><a href="#"><em class="icon ni ni-activity-alt"></em><span>Login Activity</span></a></li>
+								</ul>
+							</div>
+							<div class="dropdown-inner">
+								<ul class="link-list">
+									<li>
+										<a href="{{ route('logout') }}" onclick="
+											event.preventDefault();
+											document.getElementById('logout-form').submit();
+										">
+											<em class="icon ni ni-signout"></em><span>Sign out</span>
+										</a>
+										<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+											@csrf
+										</form>
+									</li>
+								</ul>
 							</div>
 						</div>
 					</li>
